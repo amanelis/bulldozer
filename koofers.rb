528b34064132d836596d4c2079d861e771289311
Alex Manelis <amanelis@gmail.com>
2011-11-04 22:37:04 -0700

    Got databse seeded with states

--- koofers.rb
+++ koofers.rb
@@ -21,9 +21,17 @@ puts "--------------------------------------------------------------------------
 proxies = [{:ip => '128.143.6.130', :port => '3128'}]
 
 # User agents list
-agents  = ['Windows IE 6',
-           'Windows IE 7',
-           'Mac Mozilla'] 
+agents  = ['Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6',
+           'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.84 Safari/534.13',
+           'Mozilla/5.0 (X11; U; CrOS i686 0.9.128; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.341 Safari/534.10',
+           'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)',
+           'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; Media Center PC 6.0; InfoPath.2; MS-RTC LM 8)',
+           'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6',
+           'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+           'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)',
+           'Googlebot/2.1 ( http://www.googlebot.com/bot.html)',
+           'Mozilla/2.0 (compatible; Ask Jeeves)',
+           'msnbot-Products/1.0 (+http://search.msn.com/msnbot.htm)'] 
 
 # States array to limit calls to koofers           
 # states  = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",
