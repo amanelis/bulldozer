50b10304138b638469ebec90faed7326462e517a
Alex Manelis <amanelis@gmail.com>
2011-11-05 10:28:01 -0700

    wrapped the db migration in a transaction

--- db/migrations/create_tables.rb
+++ db/migrations/create_tables.rb
@@ -20,34 +20,36 @@ ActiveRecord::Base.connection.create_database(:koofers) unless connection
 
 unless ActiveRecord::Base.connection.table_exists?(:documents)
   class CreateTables < ActiveRecord::Migration
-    create_table :documents do |t|
-      t.integer :university_id
-      t.integer :professor_id
-      t.string :course_name
-      t.string :url
-      t.string :path
-      t.string :type
-    end
+    transaction do
+      create_table :documents do |t|
+        t.integer :university_id
+        t.integer :professor_id
+        t.string :course_name
+        t.string :url
+        t.string :path
+        t.string :type
+      end
   
-    create_table :professors do |t|
-      t.integer :university_id
-      t.string :first_name
-      t.string :last_name
-      t.float :rating
-      t.integer :identifier
-      t.string :department_name
-      t.string :url
-    end
+      create_table :professors do |t|
+        t.integer :university_id
+        t.string :first_name
+        t.string :last_name
+        t.float :rating
+        t.integer :identifier
+        t.string :department_name
+        t.string :url
+      end
   
-    create_table :states do |t|
-      t.string :abbv
-    end
+      create_table :states do |t|
+        t.string :abbv
+      end
   
-    create_table :universities do |t|
-      t.string :name
-      t.string :slug
-      t.integer :state_id
-      t.string :url
+      create_table :universities do |t|
+        t.string :name
+        t.string :slug
+        t.integer :state_id
+        t.string :url
+      end
     end
   end
 end
