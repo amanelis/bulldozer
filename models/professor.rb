a6862a2b769429745bf5de6146289d587a24105b
CH <calbach88@gmail.com>
2011-11-04 23:36:28 -0700

    Model updates and create for university url.

--- models/professor.rb
+++ models/professor.rb
@@ -7,7 +7,6 @@ class Professor < ActiveRecord::Base
 
   # Creates a Professor model from the given Koofers professor URL.
   def self.create_from_url(url, university, ua)
-    # TODO(Manelis) Check if this identifier exists.
     identifier = Integer(url.match(/-(\d+)\/$/)[1])
 
     professor_identifiers = Professor.all(&:identifier)
@@ -25,8 +24,8 @@ class Professor < ActiveRecord::Base
     Net::HTTP.start(uri.host, uri.port) do |http|
       response = http.request_post(url, "jq=InstructorDetailsPage::getCourseSummary&jqargs[]=-1&jqargs[]=json&jqloggedin=0")
       result = JSON.parse(response.body)
-      fragment = Nokogiri::HTML.fragment(result['Data']['returnHTML'])
-      rating = Float(fragment.css('.summary_info div div span')[0].content)
+      fragment = Nokogiri::HTML.fragment(result["Data"]["returnHTML"])
+      rating = Float(fragment.css(".summary_info div div span")[0].content)
     end
     
     Professor.create!({:identifier => identifier, :first_name => first_name, :last_name => last_name, :rating => rating, :university => university})
