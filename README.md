615e068b8a0c95752a17be05630052cf35152fc1
Alex Manelis <amanelis@gmail.com>
2012-02-12 00:45:26 -0800

    updated scrapper to grab all contents needed from asana in new version

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 1) Build the initial database.
 	
-	$ ruby ./db/migrations/create_tables.rb
+	$ ruby db/migrations/create_tables.rb
 
 2) The first will hit Koofers entire index and retrieve all urls that will provide us with the structure to then scrape all of the exams on a faster more productive server later on. This is a multi-threaded program and is fairly fast.
 
-	$ ruby ./koofers.rb
+	$ ruby koofers.rb
 
 3) Verify that all the correct data scrapped from Koofers is correct. Once all data is in the database, you can then use that initial seed data to now scrape all of the documents. This should be run on a fast server that you can leave alone while this process runs.
 
-	$ ruby ./koofers_download.rb
+	$ ruby koofers_download.rb
 
 
 
--- document_titles.rb
+++ document_titles.rb
@@ -23,16 +23,21 @@ ActiveRecord::Base.establish_connection(
   :pool     => 75
 )
 
+# Store our threads
 threads = []
 
-documents = Document.find(:all, :conditions => ["title IS NULL"], :order => 'id ASC')
+# Queue to store the docs
 q = Queue.new
+
+# Keep track of success failure
+errors  = 0
+success = 0
+
+# Grab the documents based on no title
+documents = Document.find(:all, :conditions => ["title IS NULL"], :order => 'id ASC')
 documents.collect { |d| q << d }
 
-
-puts "Queuing up: #{documents.count} documents"
-
-
+# Number of threads
 25.times do
   threads << Thread.new(q) { |q| 
         
@@ -43,27 +48,35 @@ puts "Queuing up: #{documents.count} documents"
             page = Nokogiri::HTML(open(url))
           rescue Exception
             puts "[ERROR] Could not open the URL"
+            errors += 1
             next
           end
           
           begin 
             # Grab the css element
-            page_text = page.css('.content_header_full').first
-            text = page_text.content.match(/(.*) for/)
-            title = text.nil? ? "Document" : text[1]
+            page_title        = page.css('.content_header_full').first
+            page_category     = page.css('tr:nth-child(4) a').first
+            page_date         = page.css('tr:nth-child(6) td:nth-child(2)').first
+            page_description  = page.css('.koofer_sample_text').first
+            
+            
+            
+            title         = page_text.content
+            category      = page_category.content
+            date          = page_data.content
+            description   = page_description.content
           rescue NoMethodError
             puts "[ERROR] NoMethodError for page_text.content on doc: #{doc.id} for #{url}"
+            errors += 1
             next
           end
-
   
           # Now save the attribute
-          doc.update_attributes!(:title => title) 
+          doc.update_attributes!(:title => title, :category => page_category, :term => date, :description => description) 
 
           puts "[SUCCESS] Updated title on document: #{doc.id} with title: #{doc.title}"
       end #until empty
   }
-  
 end
 
 
